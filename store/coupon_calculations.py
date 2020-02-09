b5f8f11a82a8813fb51198ebc04e7728d7ab6802
Zachary Hayes <zachary.j.hayes@gmail.com>
2020-02-09 15:14:48 -0600

    Below 10 passes

--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -1,20 +1,20 @@
 
 
 def calculate_price(price, cash_coupon, percent_coupon):
-    total_after_savings =  price - cash_coupon
-    coupon_savings = total_after_savings * percent_coupon
+    total_after_savings = price - cash_coupon
+    coupon_savings = total_after_savings * (percent_coupon / 100)
     total_after_coupon = total_after_savings - coupon_savings
-    tax = total_after_coupon * .6
+    tax = total_after_coupon * .06
     total_after_tax = total_after_coupon + tax
     if price < 10:
         shipping = 5.95
     else:
         shipping = 0
-    return total_after_tax + shipping
+    return round(total_after_tax + shipping, 2)
 
 
 def main():
-    calculate_price(23, 3, 30)
+    print(calculate_price(9, 15, 20))
 
 
 if __name__== "__main__":
--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -7,39 +7,39 @@ class MyTestCase(unittest.TestCase):
 
     def test_price_under_ten(self):
         price = calculate_price(9, 5, 10)
-        self.assertEquals(5.95, price)
+        self.assertEquals(9.77, price)
 
     def test_price_under_ten(self):
         price = calculate_price(9, 5, 15)
-        self.assertEquals(5.95, price)
+        self.assertEquals(9.55, price)
 
     def test_price_under_ten(self):
         price = calculate_price(9, 5, 20)
-        self.assertEquals(5.95, price)
+        self.assertEquals(9.34, price)
 
     def test_price_under_ten(self):
         price = calculate_price(9, 10, 10)
-        self.assertEquals(5.95, price)
+        self.assertEquals(5.0, price)
 
     def test_price_under_ten(self):
         price = calculate_price(9, 10, 15)
-        self.assertEquals(5.95, price)
+        self.assertEquals(5.05, price)
 
     def test_price_under_ten(self):
         price = calculate_price(9, 10, 20)
-        self.assertEquals(5.95, price)
+        self.assertEquals(5.1, price)
 
     def test_price_under_ten(self):
         price = calculate_price(9, 15, 10)
-        self.assertEquals(5.95, price)
+        self.assertEquals(0.23, price)
 
     def test_price_under_ten(self):
         price = calculate_price(9, 15, 15)
-        self.assertEquals(5.95, price)
+        self.assertEquals(0.54, price)
 
     def test_price_under_ten(self):
         price = calculate_price(9, 15, 20)
-        self.assertEquals(5.95, price)
+        self.assertEquals(0.86, price)
 
 if __name__ == '__main__':
     unittest.main()
