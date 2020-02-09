b511d53b5910efc84fe11dde0ebae362a732595c
Zachary Hayes <zachary.j.hayes@gmail.com>
2020-02-09 15:17:28 -0600

    Added failed tests for ten to thirty

--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -41,5 +41,42 @@ class MyTestCase(unittest.TestCase):
         price = calculate_price(9, 15, 20)
         self.assertEquals(0.86, price)
 
+    def test_price_under_between_ten_thirty(self):
+        price = calculate_price(15, 5, 10)
+        self.assertEquals(9.77, price)
+
+    def test_price_under_between_ten_thirty(self):
+        price = calculate_price(15, 5, 15)
+        self.assertEquals(9.55, price)
+
+    def test_price_under_between_ten_thirty(self):
+        price = calculate_price(15, 5, 20)
+        self.assertEquals(9.34, price)
+
+    def test_price_under_between_ten_thirty(self):
+        price = calculate_price(15, 10, 10)
+        self.assertEquals(5.0, price)
+
+    def test_price_under_between_ten_thirty(self):
+        price = calculate_price(15, 10, 15)
+        self.assertEquals(5.05, price)
+
+    def test_price_under_between_ten_thirty(self):
+        price = calculate_price(15, 10, 20)
+        self.assertEquals(5.1, price)
+
+    def test_price_under_between_ten_thirty(self):
+        price = calculate_price(15, 15, 10)
+        self.assertEquals(0.23, price)
+
+    def test_price_under_between_ten_thirty(self):
+        price = calculate_price(15, 15, 15)
+        self.assertEquals(0.54, price)
+
+    def test_price_under_between_ten_thirty(self):
+        price = calculate_price(15, 15, 20)
+        self.assertEquals(0.86, price)
+
+
 if __name__ == '__main__':
     unittest.main()
