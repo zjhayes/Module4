0412920591c7943bd8ea695edf48ebbf9e20e423
Zachary Hayes <zachary.j.hayes@gmail.com>
2020-02-09 14:43:47 -0600

    Added main method

--- input_validation/validation_with_try.py
+++ input_validation/validation_with_try.py
@@ -6,3 +6,16 @@ def average(number1, number2, number3):
         raise ValueError
     elif number3 < 0:
         raise ValueError
+    else:
+        return (number1 + number2 + number3) / 3
+
+
+def main():
+    try:
+        print(average(90, -76, 88))
+    except ValueError:
+        print("Invalid input, must be positive number.")
+
+
+if __name__== "__main__":
+    main()
