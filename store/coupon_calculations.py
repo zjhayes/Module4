8a99380e8efff0c72305e4e42e64aa26a9195e52
Zachary Hayes <zachary.j.hayes@gmail.com>
2020-02-09 15:07:18 -0600

    filled out calculate_price

--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -1,9 +1,16 @@
 
 
 def calculate_price(price, cash_coupon, percent_coupon):
+    total_after_savings =  price - cash_coupon
+    coupon_savings = total_after_savings * percent_coupon
+    total_after_coupon = total_after_savings - coupon_savings
+    tax = total_after_coupon * .6
+    total_after_tax = total_after_coupon + tax
     if price < 10:
         shipping = 5.95
-        return 0
+    else:
+        shipping = 0
+    return total_after_tax + shipping
 
 
 def main():
